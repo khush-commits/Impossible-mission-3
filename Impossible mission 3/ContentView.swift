ff2d94d138e8c86c4b83839d2f020a5421275f25
khush-commits <khush_pareek@s2025.ssts.edu.sg>
2025-09-01 15:51:57 +0800

    Initial Commit

---
+++ Impossible mission 3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Impossible mission 3
+//
+//  Created by Khush Pareek on 1/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Impossible mission 3/Impossible_mission_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Impossible_mission_3App.swift
+//  Impossible mission 3
+//
+//  Created by Khush Pareek on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Impossible_mission_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
